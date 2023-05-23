2b31b99941cc33384c0f6d83842d96e2ca3c3f7c
Wonmi <wonmadesign@gmail.com>
2023-05-23 10:06:17 -0500

    Coded MergeSort third time.

--- Sorting/MergeSort.js
+++ Sorting/MergeSort.js
@@ -1,10 +1,40 @@
 // Merge Sort
 
-// mergeSort_1, mergeSort_2는 내가 연습한 것
-// mergeSort_3은 im-developer.tistory.com/134 님의 방식으로 다시해본것
+// mergeSort_1, mergeSort_2는 분할 시 splice를 사용하였고, 병합 시 shift를 사용하였다.
+// mergeSort_3은 분할 시 slice를 사용하였다 (im-developer.tistory.com/134 님의 방식 참고)
 
-function mergeSort_3() {
+function mergeSort_3(arr) {
+  if(arr.length < 2) return arr; // 마지막 recursive call에서는 최소 규모의 input(즉, array의 요소 값이 1개일때)을 만들어낸 후 종료.
+  
+  // 나머지 recursive call에서는 주어진 배열을 2등분한 후 combine한 것을 return. 결국 재귀적으로 했을 때 최종 리턴하는 것은 combine된 가장 큰 좌, 우 덩어리임.
+  let mid = Math.floor(arr.length/2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+  
+  return combine(mergeSort_3(left), mergeSort_3(right)); //좌측과 우측을 합치되 어떤 로직을 통해서 합칠지를 생각해서 짜야한다.
+  
+  function combine(leftArr, rightArr){
+    let resultArray = [];
+    let i = 0; //the index of left Arr
+    let j = 0; //the index of right Arr
+  
+    while (i < left.length && j < right.length) {
+      if(leftArr[i] <= rightArr[j]) {
+        resultArray.push(leftArr[i]);
+        i++;
+      } else {
+        resultArray.push(rightArr[j]);
+        j++;
+      }
+    }
 
+    return resultArray.concat(leftArr.slice(i), rightArr.slice(j)); 
+    //참고1. 좌측이나 우측 둘 중 하나는 '마지막 인덱스+1'이 되므로 [](empty array)일 것이다. 
+    //처음에 틀린 코드:
+    //return resultArray.concat(leftArr[i], rightArr[j]); 
+    //이유: 예를 들어 rightArr[last index + 1]를 레퍼런스하면 undefined가 나오게 된다. 
+    //수정: rightArr.slice(last index + 1)를 하면 []가 나오게 된다. 
+  }
 }
 
 function mergeSort_2(array) {
@@ -57,6 +87,10 @@ function mergeSort_1(array) {
 // console.timeEnd('mergeSort_1');
 
 
-console.time('mergeSort_2');
-console.log(mergeSort_2([7, 2, 10, 3, 6, 1])); // 0.294ms
-console.timeEnd('mergeSort_2');
+// console.time('mergeSort_2');
+// console.log(mergeSort_2([7, 2, 10, 3, 6, 1])); // 0.294ms
+// console.timeEnd('mergeSort_2');
+
+console.time('mergeSort_3');
+console.log(mergeSort_3([7, 2, 10, 3, 6, 1])); // 0.294ms
+console.timeEnd('mergeSort_3');
