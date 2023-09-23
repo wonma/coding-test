d5867a624b5c034d8b4c99501aa806dc5b89d8b4
Wonmi <wonmadesign@gmail.com>
2023-09-22 23:55:10 -0500

    Solved the wherefore art thou problem.

--- FCC_Challenges/whereforeArtThou.js
+++ FCC_Challenges/whereforeArtThou.js
@@ -1,22 +1,26 @@
+// ---------------- My Own Solution ----------------- //
+// 09/22/2023
+
 function whatIsInAName(collection, source) {
     const newArr = [];
-    for (let i = 0; i < collection.length; i++) {
-        // Check if the source's property exists in the collection item
-        let foundMismatch = false;
-        for (const prop in source) {
-            if (collection[i].hasOwnProperty[prop]) {
-                let isMatched = collection[i][prop] === source[prop];
-                if (!isMatched) {
-                    foundMismatch = true;
-                }
-            }
+    for (let i = 0; i < collection.length; i++) { // Investigate all the objects in the collection
+      let foundMismatch = false; // Prepare a dropout checker
+  
+      for (const sourceProp in source) { // Set a loop for all properties
+        if(!foundMismatch) { // Move on to comparision only if the source's property exists in collection
+          if(collection[i][sourceProp] !== source[sourceProp]) {
+            foundMismatch = true; // Mark a dropout for the property
+          }
         }
-        if(!foundMismatch) {
-            newArr.push(collection[i]);
-        }
+      }
+      if(!foundMismatch){ // Allow the object to pass if it didn't get any dropout markups
+        newArr.push(collection[i]);
+      }
     }
     return newArr;
-}
+  }
+
+
 
 console.log(whatIsInAName([
     { first: "Romeo", last: "Montague" }, 
@@ -25,4 +29,10 @@ console.log(whatIsInAName([
 ], 
 { last: "Capulet" }))
 
+console.log(whatIsInAName([
+    { middle: "William", last: "Thompson", first: "Josh" }, 
+    { first: "Mercutio", last: null }, 
+    { first: "Tybalt", last: "Capulet" }
+], 
+{ middle:"William", first: "Josh" }))
 
