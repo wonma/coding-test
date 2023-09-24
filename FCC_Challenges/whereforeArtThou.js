377074ad7c98a6961de64d14e87ff1ed45553b04
Wonmi <wonmadesign@gmail.com>
2023-09-23 23:50:43 -0500

    Solved wherefore art thou problem using filter method.

--- FCC_Challenges/whereforeArtThou.js
+++ FCC_Challenges/whereforeArtThou.js
@@ -22,17 +22,47 @@ function whatIsInAName(collection, source) {
 
 
 // ---------------- Example Solution 01 ----------------- //
+// Using .filter() and Object.keys()
+// The point of this solution is to leverage the power of 'return' as a means of stopping 
+// the loop for the object that doesn't have the property or doesn't have the property with a matched value.
+
+function whatIsInAName2(collection, source) {
+
+  // Code below didn't work as expected because the loop didn't get broken with return.
+  // let sourceKeys = Object.keys(source);
+  // return collection.filter(obj => {
+  //   sourceKeys.forEach(sourceKey => {
+  //     if(obj[sourceKey] !== source[sourceKey]) {
+  //       return false;
+  //     }
+  //   })
+  //   return true;
+  // })
+
+  // Code below works as expected because the 'return false' is applied to the filter method
+  // which consequently discontinue the for loop of the 'obj'.
+  let sourceKeys = Object.keys(source);
+
+  return collection.filter(obj => {
+    for(let i = 0; i < sourceKeys.length; i++) {
+      if(obj[sourceKeys[i]] !== source[sourceKeys[i]]){
+        return false;
+      }
+    }
+    return true;
+  })
+
+}
 
 
+// console.log(whatIsInAName([
+//     { first: "Romeo", last: "Montague" }, 
+//     { first: "Mercutio", last: null }, 
+//     { first: "Tybalt", last: "Capulet" }
+// ], 
+// { last: "Capulet" }))
 
-console.log(whatIsInAName([
-    { first: "Romeo", last: "Montague" }, 
-    { first: "Mercutio", last: null }, 
-    { first: "Tybalt", last: "Capulet" }
-], 
-{ last: "Capulet" }))
-
-console.log(whatIsInAName([
+console.log(whatIsInAName2([
     { middle: "William", last: "Thompson", first: "Josh" }, 
     { first: "Mercutio", last: null }, 
     { first: "Tybalt", last: "Capulet" }
