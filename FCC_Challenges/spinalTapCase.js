674c725da90caf0ee04bd0c09fa8338f89d48e52
Wonmi <wonmadesign@gmail.com>
2023-09-26 22:47:24 -0500

    Solved Sinal Tap Case problem.

--- FCC_Challenges/spinalTapCase.js
+++ FCC_Challenges/spinalTapCase.js
@@ -1,21 +1,17 @@
 function spinalCase(str) {
-    const isUpperCase = (string) => /^[A-Z]*$/.test(string)
-    const isLowerCase = (string) => /^[a-z]*$/.test(string)
+    let newStr = str.replace(/([a-z])([A-Z])/g, '$1 $2');
 
-    const strArr = str.split('');
-    const newStrArr = [];
-    for(let i = 0; i < strArr.length; i++) {
-        if(strArr[i] === ' ' || '_' || '-') {
-            newStrArr.push('-');
-        } else if(strArr.indexOf(strArr[i]) !== 0 && isUpperCase(strArr[i])) {
-            newStrArr.push(`-${strArr[i].toLowerCase()}`)
-        } else {
-            newStrArr.push(strArr[i]);
-        }
-    }
-
-    return newStrArr.join('');
+    return newStr.toLowerCase().replace(/[_|\s]+/g, '-');
 }
 
-console.log(spinalCase('Thsi Is Spinal Tap'));
+console.log(spinalCase('This Is_SpinalCoolTap'));
+
+
+function spinalCase2(str) {
+    let newStr = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+    return newStr.toLowerCase().split(/[\s|_]+/).join('-')
+}
+
+console.log(spinalCase2('This Is_SpinalCoolTap'));
 
