2359f43648dec863b1a9fd42c514c99847209f77
wonma <wonmadesign@gmail.com>
2023-10-08 23:26:43 -0500

    Solved the Sum All Primes problem on FCC.

--- FCC_Challenges/sumAllPrimes.js
+++ FCC_Challenges/sumAllPrimes.js
@@ -1,5 +1,30 @@
 function sumPrimes(num) {
-    return num;
+  // Create a helper function to check primality
+  function isPrime(num) {
+    // Get the square foot of the number
+    let sqrt = Math.sqrt(num);
+
+    // Starting from 2 (3, 4, ...) until the natural number smaller than the sqrt,
+    // check if the number has any remainder.
+    for (let i = 2; i <= sqrt; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
   }
-  
-console.log(sumPrimes(10))
+
+  let result = 0;
+  // Using the helper function and starting from 2,
+  // Check if the number is prime.
+  // If it's prime, update the 'result' variable by adding the number to it.
+
+  for (let j = 2; j <= num; j++) {
+    if (isPrime(j)) {
+      result += j;
+    }
+  }
+  return result;
+}
+
+console.log(sumPrimes(10));
