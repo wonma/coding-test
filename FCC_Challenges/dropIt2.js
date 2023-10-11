c42eb1bea11a16eeabe294608b84695ad5d67f4c
Wonmi <wonmadesign@gmail.com>
2023-10-11 03:40:50 -0500

    Solved the Drop it problem using the while loop and recursive function

--- FCC_Challenges/dropIt2.js
+++ FCC_Challenges/dropIt2.js
@@ -1,18 +1,58 @@
-// Use 'filter()'
+// 1. Use 'for loop', 'break', and shift()
+
+// My trial (failed)
+// function dropElements(arr, func) {
+
+//   for(let i = 0; i < arr.length; i++) {
+//     if(!func(arr[0])){
+//       arr.shift();  // <------- This changes the length of the original array.
+//     } else {
+//       break;
+//     }
+//   }
+
+//   return arr.length ? arr : []; // <------- Unnecessary ternary operator
+// }
+
+// Solution
+// function dropElements(arr, func) {
+//   let originalLength = arr.length
+//   for(let i = 0; i < originalLength; i++) {
+//     if(!func(arr[0])){
+//       arr.shift();
+//     } else {
+//       break;
+//     }
+//   }
+
+//   return arr;
+// }
+
+
+// 2. Use 'while loop'
+
+// My trial (failed due to infinite loop)
+// function dropElements(arr, func) {
+//   while(!func(arr[0])) { // <-------- arr[0] of an empty array returns undefined,  
+//     arr.shift();         //           which makes func(arr[0]) false,
+//   }                      //           which makes the loop keep going infinitely.
+//   return arr;
+// }
+
+
+// 2. Use a recursive function
+
+// My trial (failed)
+// function dropElements(arr, func) {
+//   return arr.length > 0 && !func(arr[0])
+//   ? dropElements(arr.slice(1), func) : []; // <----- the result of arr.slice(1) 
+// }                                          //        is used as a new arr
 
 function dropElements(arr, func) {
-  // Write here.
+  return arr.length > 0 && !func(arr[0])
+  ? dropElements(arr.slice(1), func) : arr;
 }
 
-
-// Use 'shift();
-
-// Use 'for loop' and 'br
-eak'
-
-
 console.log(
-  dropElements([1, 2, 3, 4], function(n) {
-    return n >= 3;
-  })
+  dropElements([0, 1, 0, 1], function(n) {return n === 1;})
 );
