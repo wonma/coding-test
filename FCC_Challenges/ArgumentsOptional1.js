72f1bba7315f816cd1b108d6408427a868081c2f
wonma <wonmadesign@gmail.com>
2023-10-14 23:40:00 -0500

    Saved my solution to the Arguments Optional problem.

--- FCC_Challenges/ArgumentsOptional1.js
+++ FCC_Challenges/ArgumentsOptional1.js
@@ -3,14 +3,13 @@ function addTogether() {
     return typeof value === 'number';
   }
   if (arguments.length == 2) {
-    !isNumber(arguments[0]) || !isNumber(arguments[1])
+    return !isNumber(arguments[0]) || !isNumber(arguments[1])
       ? undefined
       : arguments[0] + arguments[1];
   } else if (arguments.length < 2) {
-    return input => {
-      return !isNumber(input) ? undefined : arguments[0] + input;
-    };
-  }
+      return !isNumber(input) ? undefined : input => {
+          return arguments[0] + input;
+      }
 }
 
 console.log(addTogether(5)(7));
