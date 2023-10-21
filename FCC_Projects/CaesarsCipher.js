f338c9d9e48057348cc706d8295aead786d74aa0
Wonmi <wonmadesign@gmail.com>
2023-10-20 22:40:54 -0500

    Solved the CaesarsCipher problem on FCC.

--- FCC_Projects/CaesarsCipher.js
+++ FCC_Projects/CaesarsCipher.js
@@ -1,5 +1,20 @@
 function rot13(str) {
-  return str;
+  let strArr = str.split('')
+   
+  let convertedArr = strArr.map((letter) => {
+
+    const isLetter = /[A-Za-z]/g
+
+    if(isLetter.test(letter)) {
+      let code = letter.charCodeAt(0)
+      return code <= 77 ? 
+      String.fromCharCode(code+13) 
+      : String.fromCharCode(code-13);
+    } else {
+      return letter
+    }
+  })
+  return convertedArr.join('')
 }
 
-rot13('SERR PBQR PNZC');
+console.log(rot13("SERR PBQR PNZC"))
