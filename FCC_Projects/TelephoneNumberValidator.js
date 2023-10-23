7d96377db147fbe08d319abdb98830b85e02511b
wonma <wonmadesign@gmail.com>
2023-10-22 21:31:28 -0500

    Solved the Telephone Number Validator

--- FCC_Projects/TelephoneNumberValidator.js
+++ FCC_Projects/TelephoneNumberValidator.js
@@ -1,41 +1,42 @@
 function telephoneCheck(str) {
-    //일단 사전 작업으로 white space지우기
-    let trimmed = str.replace(/\s/, '');
-  
-    let findAllNumbers = /[0-9]/g;
-    let lengthOfNums = trimmed.match(findAllNumbers).length;
-    console.log(lengthOfNums)
-  
-    // 특수문자가 있는 경우 바로 false 리턴
-    let hasSpecials = /[^0-9\-\s]/g;
-    if(hasSpecials.test(trimmed)) return false;
-    
-    // 숫자가 10미만이거나 12이상인경우 바로 false 리턴
-    if(lengthOfNums < 10 || lengthOfNums >= 12 ) {
-      console.log('hi')
-      return false;
-    };
-  
-   //숫자 11개인데 시작이 '1'이 아니면 바로 false 리턴
-  if(lengthOfNums == 11 && trimmed[0] !== '1' ) {
-    return false;
-  };
-  
-   //숫자 11개이면 일단 앞에 '1'을 삭제하고 reassign하여 숫자 10개로 만들기
-   let cCodeRemoved = ''
-   if(lengthOfNums == 11) {
-    cCodeRemoved = trimmed.substring(1);
-    }
-  
-    let hasLiner = /[()-]/g
-    if(hasLiner.test(cCodeRemoved)) {
-      return 'hi'
-    }
-    // -혹은 괄호를 포함하는 포맷일 경우
-      // 우선 마지막 '세자리-네자리'를 지운다 
-      // a. 세자리-
-      // b. 괄호열기 세자리 괄호닫기               
+  // Remove all white spaces
+  let trimmed = str.replace(/\s/g, '');
+
+  // Get how many numbers the string has.
+  let findAllNumbers = /[0-9]/g;
+  let lengthOfNums = trimmed.match(findAllNumbers).length;
+
+  // Falsy Case 1.
+  // Return false if the string contains any characters other than a number, -, (, and ).
+  if (/[^0-9\-\(\)]/g.test(trimmed)) return false;
+
+  // Falsy Case 2.
+  // Return false if the number of numbers are either less than 10 or more than 11.
+  if (lengthOfNums < 10 || lengthOfNums >= 12) return false;
+
+  // Falsy Case 3.
+  // If the number of the numbers in the string is 11 and
+  // it does not start with '1', return false.
+  if (lengthOfNums == 11 && trimmed[0] !== '1') return false;
+
+  // Simplify Step 1.
+  // Remove the first number '1' country code if the number of the numbers is 11.
+  if (lengthOfNums == 11) {
+    trimmed = trimmed.slice(1);
   }
-  
-  // telephoneCheck("555-555-5555");
-  console.log(telephoneCheck('1 555-555-5555'))
+
+  // Now we have the full phone number without the country code.
+  // If it contains '-' or parenthesis, it should start with either '(555)' or '555-'.
+  let hasLiner = /[\(\)-]/g;
+  if (hasLiner.test(trimmed)) {
+    const patternA = /^[0-9]{3}-/;
+    const patternB = /^\([0-9]{3}\)/;
+    return patternA.test(trimmed) || patternB.test(trimmed) ? true : false;
+  } else {
+    // If it consists of 10 digits only, there's nothing more to check, so we can return true.
+    return true;
+  }
+}
+
+// telephoneCheck("555-555-5555");
+console.log(telephoneCheck('1555-555-5555'));
